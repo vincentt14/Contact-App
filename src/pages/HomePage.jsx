e55f53c71f9d7d49baa34c607cd1c4bae7472987
vincentt14 <vincent14@student.umn.ac.id>
2022-10-03 00:49:58 +0700

    v03
    fitur search dengan query parameter sudah ditambahkan dan penambahan react-icon

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,20 @@
 import React from "react";
 import ContactList from "../components/ContactList";
 import { deleteContact, getContacts } from "../utils/data";
+import SearchBar from "../components/SearchBar";
+import { useSearchParams } from "react-router-dom";
+
+function HomePageWrapper() {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const keyword = searchParams.get("keyword");
+
+  function changeSearchParams(keyword) {
+    setSearchParams({ keyword });
+  }
+
+  return <HomePage defaultKeyword={keyword} keywordChange={changeSearchParams} />;
+}
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -8,9 +22,20 @@ class HomePage extends React.Component {
 
     this.state = {
       contacts: getContacts(),
+      keyword: props.defaultKeyword || "",
     };
 
     this.onDeleteEventHandler = this.onDeleteEventHandler.bind(this);
+    this.onKeywordChangeHandler = this.onKeywordChangeHandler.bind(this);
+  }
+
+  onKeywordChangeHandler(keyword) {
+    this.setState(() => {
+      return {
+        keyword,
+      };
+    });
+    this.props.keywordChange(keyword);
   }
 
   onDeleteEventHandler(id) {
@@ -25,13 +50,18 @@ class HomePage extends React.Component {
   }
 
   render() {
+    const contacts = this.state.contacts.filter((contact) => {
+      return contact.name.toLowerCase().includes(this.state.keyword.toLowerCase());
+    });
+
     return (
       <section>
+        <SearchBar keyword={this.state.keyword} keywordChange={this.onKeywordChangeHandler} />
         <h2>Daftar Kontak</h2>
-        <ContactList contacts={this.state.contacts} onDelete={this.onDeleteEventHandler} />
+        <ContactList contacts={contacts} onDelete={this.onDeleteEventHandler} />
       </section>
     );
   }
 }
 
-export default HomePage;
+export default HomePageWrapper;
