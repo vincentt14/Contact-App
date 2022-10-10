8917f1a1c1558a5089e93e1d0b69e19e9a390e98
vincentt14 <vincent14@student.umn.ac.id>
2022-10-10 14:03:34 +0700

    v05
    penerapan React Context - penambahan fitur ganti bahasa

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { LocaleConsumer } from "../contexts/LocaleContext";
 
-function SearchBar({keyword, keywordChange}) {
-  return <input className="search-bar" type="text" placeholder="Cari berdasarkan nama" value={keyword} onChange={(event) => keywordChange(event.target.value)} />;
+function SearchBar({ keyword, keywordChange }) {
+  return (
+    <LocaleConsumer>
+      {({ locale }) => {
+        return <input className="search-bar" type="text" placeholder={locale === "id" ? "Cari berdasarkan nama" : "Search by name"} value={keyword} onChange={(event) => keywordChange(event.target.value)} />;
+      }}
+    </LocaleConsumer>
+  );
 }
 
 SearchBar.propTypes = {
